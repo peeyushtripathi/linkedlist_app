09f6c10eee71063cc380435ebe85de055b721afa
peeyushtripathi <peeyushtripathi9392@gmail.com>
2023-08-16 02:51:08 +0530

    added imput.txt file

--- linked_list.c
+++ linked_list.c
@@ -1,8 +1,49 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+typedef struct ll
+{
+	struct ll *next;
+	int data;
+}ll;
+
+void insert(ll *head)
+{
+	FILE *fp = fopen("input.txt", "r");
+	if (fp== NULL)
+	{
+		printf("file not present \n");
+		return;
+	}
+	else {
+		if (head==NULL)
+		{
+			ll *ptr =head;
+			ll *ptrbkp=ptr;
+		}
+		else
+		{
+			while(head->next !=NULL)
+			{
+				ll *ptrbkp=head;
+				head=head->next;
+			}
+			ll *preptr=head;
+		}
+		{
+			int number=0;
+			while(fscanf(fp, "%d",&number) == 1)
+			{
+        			printf("%d\n", number);
+      			}
+			fclose(fp);
+		}
+		}
+}
+
 int main()
 {
+	insert(NULL);
 	printf("inside main \n");
 	return 0;
 }
